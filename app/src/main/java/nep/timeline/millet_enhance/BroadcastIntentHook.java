f86fbf2e4e65f6726587b0b0bf4e38be7527198b
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2025-05-28 00:50:56 +0800

    fix nullptr

--- app/src/main/java/nep/timeline/millet_enhance/BroadcastIntentHook.java
+++ app/src/main/java/nep/timeline/millet_enhance/BroadcastIntentHook.java
@@ -13,77 +13,79 @@ import de.robv.android.xposed.XposedHelpers;
 
 public class BroadcastIntentHook {
     public BroadcastIntentHook(ClassLoader classLoader) {
-       try {
-           Class<?> clazz = XposedHelpers.findClassIfExists("com.android.server.am.ActivityManagerService", classLoader);
-           if (Build.VERSION.SDK_INT > Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-               Class<?> controller = XposedHelpers.findClassIfExists("com.android.server.am.BroadcastController", classLoader);
-               if (controller != null)
-                   clazz = controller;
-           }
+        try {
+            Class<?> clazz = XposedHelpers.findClassIfExists("com.android.server.am.ActivityManagerService", classLoader);
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                Class<?> controller = XposedHelpers.findClassIfExists("com.android.server.am.BroadcastController", classLoader);
+                if (controller != null)
+                    clazz = controller;
+            }
 
-           if (clazz == null) {
-               XposedBridge.log(GlobalVars.TAG + " -> Failed to listen broadcast intent!");
-               return;
-           }
+            if (clazz == null) {
+                XposedBridge.log(GlobalVars.TAG + " -> Failed to listen broadcast intent!");
+                return;
+            }
 
-           Method targetMethod = null;
-           for (Method method : clazz.getDeclaredMethods())
-               if (method.getName().equals("broadcastIntentLocked") && (targetMethod == null || targetMethod.getParameterTypes().length < method.getParameterTypes().length))
-                   targetMethod = method;
+            Method targetMethod = null;
+            for (Method method : clazz.getDeclaredMethods())
+                if (method.getName().equals("broadcastIntentLocked") && (targetMethod == null || targetMethod.getParameterTypes().length < method.getParameterTypes().length))
+                    targetMethod = method;
 
-           ArrayList<Object> arrayList = new ArrayList<>(Arrays.asList(targetMethod.getParameterTypes()));
-           arrayList.add(new XC_MethodHook() {
-               @Override
-               protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) {
-                   try {
-                       int intentArgsIndex = 2;
-                       if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q)
-                           intentArgsIndex = 3;
+            ArrayList<Object> arrayList = new ArrayList<>(Arrays.asList(targetMethod.getParameterTypes()));
+            arrayList.add(new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) {
+                    try {
+                        int intentArgsIndex = 2;
+                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q)
+                            intentArgsIndex = 3;
 
-                       int userIdIndex = 15;
-                       if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P)
-                           userIdIndex = 17;
-                       if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R)
-                           userIdIndex = 18;
-                       if (Build.VERSION.SDK_INT == Build.VERSION_CODES.S)
-                           userIdIndex = 19;
-                       if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2 && Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
-                           userIdIndex = 20;
-                       if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU)
-                           userIdIndex = 21;
+                        int userIdIndex = 15;
+                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P)
+                            userIdIndex = 17;
+                        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R)
+                            userIdIndex = 18;
+                        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.S)
+                            userIdIndex = 19;
+                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2 && Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+                            userIdIndex = 20;
+                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU)
+                            userIdIndex = 21;
 
-                       Intent intent = (Intent) param.args[intentArgsIndex];
-                       int userId = (int) param.args[userIdIndex];
-                       if (intent != null) {
-                           String action = intent.getAction();
+                        Intent intent = (Intent) param.args[intentArgsIndex];
+                        int userId = (int) param.args[userIdIndex];
+                        if (intent != null) {
+                            String action = intent.getAction();
+                            if (action == null)
+                                return;
 
-                           boolean isFCM = action.endsWith(".android.c2dm.intent.RECEIVE");
-                           boolean isUnified = "org.unifiedpush.android.connector.MESSAGE".equals(action);
+                            boolean isFCM = action.endsWith(".android.c2dm.intent.RECEIVE") || "com.google.firebase.MESSAGING_EVENT".equals(action) || "com.google.firebase.INSTANCE_ID_EVENT".equals(action);
+                            boolean isUnified = "org.unifiedpush.android.connector.MESSAGE".equals(action);
 
-                           if (action == null || (!isFCM && !isUnified))
-                               return;
+                            if (!isFCM && !isUnified)
+                                return;
 
-                           String packageName = (intent.getComponent() == null ? intent.getPackage() : intent.getComponent().getPackageName());
+                            String packageName = (intent.getComponent() == null ? intent.getPackage() : intent.getComponent().getPackageName());
 
-                           if (packageName == null)
-                               return;
+                            if (packageName == null)
+                                return;
 
-                           int uid = ActivityManagerService.getUidFromPackage(packageName, userId);
-                           if (uid != -1 && GreezeManagerService.isUidFrozen(uid))
-                               GreezeManagerService.thawUid(uid, 1000, isFCM ? "FCM" : "Unified");
-                       }
-                   } catch (Throwable throwable) {
-                       XposedBridge.log(GlobalVars.TAG + " -> Throw exception:");
-                       XposedBridge.log(throwable);
-                   }
-               }
-           });
+                            int uid = ActivityManagerService.getUidFromPackage(packageName, userId);
+                            if (uid != -1 && GreezeManagerService.isUidFrozen(uid))
+                                GreezeManagerService.thawUid(uid, 1000, isFCM ? "FCM" : "Unified");
+                        }
+                    } catch (Throwable throwable) {
+                        XposedBridge.log(GlobalVars.TAG + " -> Throw exception:");
+                        XposedBridge.log(throwable);
+                    }
+                }
+            });
 
-           XposedHelpers.findAndHookMethod(clazz, targetMethod.getName(), arrayList.toArray());
+            XposedHelpers.findAndHookMethod(clazz, targetMethod.getName(), arrayList.toArray());
 
-           XposedBridge.log(GlobalVars.TAG + " -> Listen broadcast intent");
-       } catch (Throwable throwable) {
-           XposedBridge.log(GlobalVars.TAG + " -> Failed to listen broadcast because: " + throwable.getMessage());
-       }
+            XposedBridge.log(GlobalVars.TAG + " -> Listen broadcast intent");
+        } catch (Throwable throwable) {
+            XposedBridge.log(GlobalVars.TAG + " -> Failed to listen broadcast because: " + throwable.getMessage());
+        }
     }
 }
